46e13d7fc8b911bd05e9ef6c0b7dcacbdb2872a8
andrewseo2001 <andrewseo2001@gmail.com>
2024-02-14 20:19:26 -0500

    app front end can build

--- App.js
+++ App.js
@@ -10,6 +10,11 @@ import {
 import { BleManager } from 'react-native-ble-plx';
 import { useEffect, useState, useRef } from 'react';
 import { atob } from 'react-native-quick-base64';
+import Bottomnav from "./components/Bottomnav"
+import {DefaultTheme, Provider as PaperProvider} from "react-native-paper";
+// import firebase from "firebase/app";
+import TopBar from "./components/TopBar";
+// import {firebaseConfig} from "./firebaseConfig";
 
 const bleManager = new BleManager();
 const SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
@@ -73,116 +78,157 @@ async function requestPermission(){
 
 requestPermission();
 
-
 export default function App() {
-  const [deviceID, setDeviceID] = useState(null);
-  const [stepCount, setStepCount] = useState(0);
-  const [stepDataChar, setStepDataChar] = useState(null); // Not Used
-  const [connectionStatus, setConnectionStatus] = useState("Searching...");
 
-  const progress = (stepCount / 1000) * 100;
-
-  const deviceRef = useRef(null);
-
-  const searchAndConnectToDevice = () => {
-    bleManager.startDeviceScan(null, null, (error, device) => {
-      if (error) {
-        console.error(error);
-        setConnectionStatus("Error searching for devices");
-        return;
-      }
-      if (device.name === "Smart-sip") {
-        bleManager.stopDeviceScan();
-        setConnectionStatus("Connecting...");
-        connectToDevice(device);
-      }
-    });
+  const theme = {
+      ...DefaultTheme,
+      dark: true,
+      mode: 'adaptive',
+      roundness: 15,
+      colors: {
+          ...DefaultTheme.colors,
+          primary: '#2176FF',
+          accent: '#33A1FD',
+          surface: '#131A26',
+          background: '#131A26',
+          text: '#FFFFFF'
+      },
   };
-
-  useEffect(() => {
-    searchAndConnectToDevice();
-  }, []);
-
-  const connectToDevice = (device) => {
-    return device
-      .connect()
-      .then((device) => {
-        setDeviceID(device.id);
-        setConnectionStatus("Connected");
-        deviceRef.current = device;
-        return device.discoverAllServicesAndCharacteristics();
-      })
-      .then((device) => {
-        return device.services();
-      })
-      .then((services) => {
-        let service = services.find((service) => service.uuid === SERVICE_UUID);
-        return service.characteristics();
-      })
-      .then((characteristics) => {
-        let stepDataCharacteristic = characteristics.find(
-          (char) => char.uuid === STEP_DATA_CHAR_UUID
-        );
-        setStepDataChar(stepDataCharacteristic);
-        stepDataCharacteristic.monitor((error, char) => {
-          if (error) {
-            console.error(error);
-            return;
-          }
-          const rawStepData = atob(char.value);
-          console.log("Received step data:", rawStepData);
-          setStepCount(rawStepData);
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-        setConnectionStatus("Error in Connection");
-      });
-  };
-
-  useEffect(() => {
-    const subscription = bleManager.onDeviceDisconnected(
-      deviceID,
-      (error, device) => {
-        if (error) {
-          console.log("Disconnected with error:", error);
-        }
-        setConnectionStatus("Disconnected");
-        console.log("Disconnected device");
-        setStepCount(0); // Reset the step count
-        if (deviceRef.current) {
-          setConnectionStatus("Reconnecting...");
-          connectToDevice(deviceRef.current)
-            .then(() => setConnectionStatus("Connected"))
-            .catch((error) => {
-              console.log("Reconnection failed: ", error);
-              setConnectionStatus("Reconnection failed");
-            });
-        }
-      }
-    );
-    return () => subscription.remove();
-  }, [deviceID]);
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your apps!</Text>
-      <Text>{stepCount}</Text>
-      <Text>{connectionStatus}</Text>
-
-      <StatusBar style="auto" />
-    </View>
+      <PaperProvider theme={theme}>
+          <View style={styles.container}>
+              <View style={styles.content}>
+                  <TopBar/>
+                  <Bottomnav/>
+              </View>
+              <StatusBar style="auto" />
+          </View>
+      </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      backgroundColor: '#fff',
+      justifyContent: 'center',
   },
+  content: {
+      flex: 10,
+  }
 });
+
+
+// export default function App() {
+//   const [deviceID, setDeviceID] = useState(null);
+//   const [stepCount, setStepCount] = useState(0);
+//   const [stepDataChar, setStepDataChar] = useState(null); // Not Used
+//   const [connectionStatus, setConnectionStatus] = useState("Searching...");
+
+//   const progress = (stepCount / 1000) * 100;
+
+//   const deviceRef = useRef(null);
+
+//   const searchAndConnectToDevice = () => {
+//     bleManager.startDeviceScan(null, null, (error, device) => {
+//       if (error) {
+//         console.error(error);
+//         setConnectionStatus("Error searching for devices");
+//         return;
+//       }
+//       if (device.name === "Smart-sip") {
+//         bleManager.stopDeviceScan();
+//         setConnectionStatus("Connecting...");
+//         connectToDevice(device);
+//       }
+//     });
+//   };
+
+//   useEffect(() => {
+//     searchAndConnectToDevice();
+//   }, []);
+
+//   const connectToDevice = (device) => {
+//     return device
+//       .connect()
+//       .then((device) => {
+//         setDeviceID(device.id);
+//         setConnectionStatus("Connected");
+//         deviceRef.current = device;
+//         return device.discoverAllServicesAndCharacteristics();
+//       })
+//       .then((device) => {
+//         return device.services();
+//       })
+//       .then((services) => {
+//         let service = services.find((service) => service.uuid === SERVICE_UUID);
+//         return service.characteristics();
+//       })
+//       .then((characteristics) => {
+//         let stepDataCharacteristic = characteristics.find(
+//           (char) => char.uuid === STEP_DATA_CHAR_UUID
+//         );
+//         setStepDataChar(stepDataCharacteristic);
+//         stepDataCharacteristic.monitor((error, char) => {
+//           if (error) {
+//             console.error(error);
+//             return;
+//           }
+//           const rawStepData = atob(char.value);
+//           console.log("Received step data:", rawStepData);
+//           setStepCount(rawStepData);
+//         });
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//         setConnectionStatus("Error in Connection");
+//       });
+//   };
+
+//   useEffect(() => {
+//     const subscription = bleManager.onDeviceDisconnected(
+//       deviceID,
+//       (error, device) => {
+//         if (error) {
+//           console.log("Disconnected with error:", error);
+//         }
+//         setConnectionStatus("Disconnected");
+//         console.log("Disconnected device");
+//         setStepCount(0); // Reset the step count
+//         if (deviceRef.current) {
+//           setConnectionStatus("Reconnecting...");
+//           connectToDevice(deviceRef.current)
+//             .then(() => setConnectionStatus("Connected"))
+//             .catch((error) => {
+//               console.log("Reconnection failed: ", error);
+//               setConnectionStatus("Reconnection failed");
+//             });
+//         }
+//       }
+//     );
+//     return () => subscription.remove();
+//   }, [deviceID]);
+
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your apps!</Text>
+//       <Text>{stepCount}</Text>
+//       <Text>{connectionStatus}</Text>
+
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
 
 // const styles = StyleSheet.create({
